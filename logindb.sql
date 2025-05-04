fa5631010bf0b220580940dd51f1d7d561dd4471
mmd-amini8708 <mmd.aamini.08@gmail.com>
2025-05-04 11:59:27 +0330

    commit

---
+++ logindb.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 04, 2025 at 08:27 AM
+-- Server version: 8.3.0
+-- PHP Version: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `logindb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE IF NOT EXISTS `data` (
+  `title` text NOT NULL,
+  `text` text NOT NULL,
+  `text2` text NOT NULL,
+  `row` int NOT NULL AUTO_INCREMENT,
+  `imageURL` text NOT NULL,
+  PRIMARY KEY (`row`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`title`, `text`, `text2`, `row`, `imageURL`) VALUES
+('hgf', 'nbv', '60000', 11, 'images/amino.jpg'),
+('amino', 'fghj', '51000', 13, 'images/amino.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int DEFAULT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `Admin` int NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `Admin`) VALUES
+(NULL, 'sexyman', 'sex123', 'sex@gmail.com', 0),
+(NULL, 'sexyman', 'sex123', 'sex@gmail.com', 0),
+(NULL, 'sexyman', 'sex123', 'sex@gmail.com', 0),
+(NULL, 'sexyman', 'sex123', 'sex@gmail.com', 0),
+(NULL, 'sexyman', 'sex123', 'sex@gmail.com', 0),
+(NULL, 'mmd', '123', 'jgvjyhn@kgbkj.com', 0),
+(NULL, 'sexyman', '123', 'sex@gmail.com', 0),
+(NULL, 'admin', '123', 'mmd.aamini.08@gmail.com', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
